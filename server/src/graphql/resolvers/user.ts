9abd144e997cc3fd1a191c219f49fc2015d76524
valgaug <valentin.gaugain@hotmail.fr>
2023-12-06 20:13:09 +0000

    hash + salt

--- server/src/graphql/resolvers/user.ts
+++ server/src/graphql/resolvers/user.ts
@@ -1,17 +1,35 @@
+import bcrypt from 'bcrypt';
 import User from '../../models/User';
 import { QueryResolvers, MutationResolvers } from '../../generated/graphql';
 
-export const queryResolvers: QueryResolvers = {
+const saltRounds = 10;
+
+const queryResolvers: QueryResolvers = {
   getUser: async (_, { email }) => {
     return await User.findOne({ email });
   },
 };
 
-export const mutationResolvers: MutationResolvers = {
+const mutationResolvers: MutationResolvers = {
   createUser: async (_, { firstName, lastName, email, password }) => {
-    const newUser = new User({ firstName, lastName, email, password });
-    // Remember to hash the password before saving
-    return await newUser.save();
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    try {
+      return await newUser.save();
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new Error('Error creating user: ' + error.message);
+      } else {
+        throw error;
+      }
+    }
   },
 };
 
