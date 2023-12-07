2e4697485281e6fe6e4d024a5a2409eaffcd6ee1
valgaug <valentin.gaugain@hotmail.fr>
2023-12-07 15:45:21 +0000

    env examples + gitignore

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.SERVER_PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
